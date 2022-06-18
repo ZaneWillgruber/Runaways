75497096d767ca46a1e4aebf96cf8f29ab1b6166
ZaneWillgruber <zanetwill@gmail.com>
2022-06-17 20:34:35 -0500

    Styling

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,29 +1,56 @@
 <template>
   <div class="grid place-items-center h-screen w-screen font-sans text-white">
-    <div class="m-auto min-w-ma grid grid-flow-row grid-cols-2 gap-4 rounded-2xl p-20 shadow-2xl bg-gradient-to-t from-indigo-500 to-blue-300 scale-150">
-      <div class="m-auto row-span-2">
+    <div class="m-auto min-w-ma grid grid-flow-row grid-cols-2 gap-4 rounded-2xl shadow-2xl bg-gradient-to-t from-indigo-500 to-blue-200 scale-150">
+      <div class="m-20 row-span-2">
         <img class="border-white border-8 rounded-full"
           src="https://bytetag-profile-pictures.s3.amazonaws.com/f965d98b-69d7-4e30-9ccd-cc00773118a2.png?pfpVersion=0VKR">
       </div>
-      <div class="m-auto">
-        <p>--------------</p>
-        <h1 class="text-xl font-bold">Oz</h1>
-        <p>Boxer Mix</p>
-      </div>
-      <div class="info m-auto">
-        <div class="m-auto">
-          <h4>Boy</h4>
-          <p>Gender</p>
+      <div class="col-span-1">
+        <div class="grid grid-cols-2 grid-flow-row gap-2 m-auto ml-0 mr-20">
+          <div class="m-auto col-span-2 ml-0">
+            <h1 class="text-xl font-bold">Oz</h1>
+            <p>Boxer Mix</p>
+          </div>
+            <div class="about">
+              <h3>About</h3>
+              <hr>
+              <p>Gender: Boy</p>
+              <p>Age: 2 years</p>
+              <p>Weight: 50lbs</p>
+            </div>
+            <div>
+              <h3>Behavior</h3>
+              <hr>
+              <p>Good With...</p>
+            </div>
+            <div>
+              <h3>Health</h3>
+              <hr>
+              <p>Allergies</p>
+              <p>Medicine</p>
+              <p>Neutered/Spayed</p>
+            </div>
+            <div>
+              <h3>Provider</h3>
+              <hr>
+              <p>Allergies</p>
+              <p>Medicine</p>
+              <p>Neutered/Spayed</p>
+            </div>
+            <!-- <div class="m-auto">
+              <h4>Boy</h4>
+              <p>Gender</p>
+            </div>
+            <div class="category">
+              <h4>2 years</h4>
+              <p>Age</p>
+            </div>
+            <div class="category">
+              <h4>50 lbs</h4>
+              <p>Weight</p>
+            </div> -->
+          </div>
         </div>
-        <div class="category">
-          <h4>2 years</h4>
-          <p>Age</p>
-        </div>
-        <div class="category">
-          <h4>50 lbs</h4>
-          <p>Weight</p>
-        </div>
-      </div>
     </div>
   </div>
 </template>
